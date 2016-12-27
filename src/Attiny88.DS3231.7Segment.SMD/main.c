f5a341cbe14758d020a18e353be028c8181d07b0
Ruslan Mikhaylenko <mirusnet@gmail.com>
2016-12-27 16:54:54 +0200

    Attiny88 DIP

--- src/Attiny88.DS3231.7Segment.SMD/main.c
+++ src/Attiny88.DS3231.7Segment.SMD/main.c
@@ -1,6 +1,6 @@
 /*
-	DS3231 clock implemented on Attiny2313A using 5641-BSR 7 segment display
-	(c) Ruslan Mikhaylenko 28.11.2016 mirusnet@gmail.com
+	DS3231 clock implemented on Attiny88 using 5611-BSR 7 segment display. Common anode.
+	(c) Ruslan Mikhaylenko 27.12.2016 mirusnet@gmail.com
 */
 
 //#define F_CPU 31250
@@ -41,27 +41,7 @@ X-X-X-X-X-X-X-D
 
 #define FIRST_ON	((PORTA) |= (1<<(PA1)))
 #define FIRST_OFF	((PORTA) &= ~(1<<(PA1)))
-#define SECOND_ON	((PORTA) |= (1<<(PA0)))
-#define SECOND_OFF	((PORTA) &= ~(1<<(PA0)))
-#define THIRD_ON	((PORTD) |= (1<<(PD2)))
-#define THIRD_OFF	((PORTD) &= ~(1<<(PD2)))
-#define FOUR_ON		((PORTD) |= (1<<(PD1)))
-#define FOUR_OFF	((PORTD) &= ~(1<<(PD1)))
 
-
-
-#define DIGIT_ONE 			0xBE
-#define DIGIT_TWO			0x62
-#define DIGIT_THREE			0x2A
-#define DIGIT_FOUR			0x3C
-#define DIGIT_FIVE			0x29
-#define DIGIT_SIX			0x21
-#define DIGIT_SEVEN			0xBA
-#define DIGIT_EIGHT			0x20
-#define DIGIT_NINE			0x28
-#define DIGIT_ZERO			0xA0
-#define DIGIT_ALL_DISABLE	0xFF
-#define DIGIT_ALL_ENABLE	0x00
 
 volatile uint8_t minutes_register	= 0;
 volatile uint8_t hours_register		= 0;
@@ -73,25 +53,19 @@ volatile uint8_t digit_hours		= 0;
 
 
 
+/*
 uint8_t convert(uint8_t digit) {
 	switch (digit) {
         case 1: return DIGIT_ONE; 	break;
-        case 2: return DIGIT_TWO; 	break;
-        case 3: return DIGIT_THREE; break;
-        case 4: return DIGIT_FOUR;  break;
-		case 5: return DIGIT_FIVE;  break;
-        case 6: return DIGIT_SIX;   break;
-        case 7: return DIGIT_SEVEN; break;
-        case 8: return DIGIT_EIGHT; break;
-		case 9: return DIGIT_NINE;  break;
         case 0: return DIGIT_ZERO;  break;
 	    default: return DIGIT_ALL_DISABLE;
     }
 }
+*/
 
+//volatile uint8_t interrupt = 0;
 
-volatile uint8_t interrupt = 0;
-
+/*
 void get_clock(void) {
 	i2c_start_wait(0xD0);					// set device address and write mode
 	i2c_write(0x01); 						// write "read from" byte
@@ -101,25 +75,19 @@ void get_clock(void) {
 	hours_register		= i2c_readNak(); 	// Read second byte and send END
 	i2c_stop();
 	
-	digit_dec_minutes	= convert((minutes_register >> 4) & B0111);
-	digit_minutes		= convert(minutes_register & B00001111);
-	digit_dec_hours		= convert((hours_register >> 4) & B1);
-	digit_hours			= convert(hours_register & B00001111);
-	BIT_CLEAR(digit_hours, PB5); 			// enable dot segment
+	digit_dec_minutes	= (minutes_register >> 4) & B0111;
+	digit_minutes		= minutes_register & B00001111;
+	digit_dec_hours		= (hours_register >> 4) & B1;
+	digit_hours			= hours_register & B00001111;
 }
-
-ISR(TIMER0_OVF_vect) {
-	interrupt++;
-	
-	if(interrupt > 100) {
-		interrupt=0;
+*/
+//ISR(TIMER0_OVF_vect) {
 		/*	We can read the clock directly in the interrupt
 			as it is being executed every 26 sec
 			and we dont have any other interrupt in place
 		*/
-		get_clock(); 
-	}
-}
+//		get_clock(); 
+//}
 
 
 
@@ -128,29 +96,35 @@ ISR(TIMER0_OVF_vect) {
 
 int main(void) {
 	//clock_prescale_set(clock_div_16); //8MHz/16 = 500KHz
-	i2c_init();
+	//i2c_init();
 	
-	ACSR|=(1<<ACD);					//Disable Analog Comparator
- 
-	DDRB = 0xFF; 					//Set all pins of PORTB as output
-	DDRD = (1<<PD2) | (1<<PD1);		//DIGITS ANODE:	PD2=>3 PD1=>4
-	DDRA = (1<<PA0) | (1<<PA1);		//DIGITS ANODE:	PA0=>2 PA1=>1
-	PORTB = 0x00;					//Ground all segments (TURN ON).
+	BIT_SET(ACSR,ACD);					// Disable Analog Comparator
+	BIT_CLEAR(ADCSRB, ADEN);			// Disable Analog to Digital Converter
 	
+	// PORT DIRECTIONS 0 INPUT 1 OUTPUT
+	DDRA = B11111011;
+	DDRB = B11111111;
+	DDRC = B11111111;
+	DDRD = B11100111;
+	
+	// PORT STATE 0 LOW 1 HIGH
+	PORTA = B00000000;
+	PORTB = B00000000;
+	PORTC = B00000000;
+	PORTD = B00001000; // Internal pull up res on PD3 for INT1 button
+	
+	
+	
+	/*
 	//8 Bit timer. Overflow routine  - ISR(TIMER0_OVF_vect)
 	TIMSK	= 1<<TOIE0;				 // Enable overflow interrupt by timer T0
 	TCCR0B	= (1<<CS00) | (1<<CS02); // Set up timer at F_CPU/1024 prescaler
 	TCNT0	= 0x00; 		 		 // Zero timer (start it)
 	sei(); 				 			 // Enable global interrupts
-
+*/
 	
 	
 	while(1) {
-		if(digit_dec_hours != DIGIT_ZERO) { // Do not show first digit if it is zero
-			PORTB = digit_dec_hours;		FIRST_ON;	_delay_ms(5);	FIRST_OFF;
-		}
-			PORTB = digit_hours;			SECOND_ON;	_delay_ms(5);	SECOND_OFF;
-			PORTB = digit_dec_minutes;		THIRD_ON;	_delay_ms(5);	THIRD_OFF;
-			PORTB = digit_minutes;			FOUR_ON;	_delay_ms(5);	FOUR_OFF;
+	
 	}
 }
