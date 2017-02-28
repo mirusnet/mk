37733a985bd8d7dae6e21c244d53a1b2882178fb
Ruslan Mikhaylenko <mirusnet@gmail.com>
2017-02-28 12:56:53 +0200

    Multiplex adding Atmel Studio

---
+++ src/Attiny88.DS3231.7Segment.SMD.Multiplex/main.c
@@ -0,0 +1,18 @@
+/*
+ * Attiny88.DS3231.7Segment.SMD.Multiplex.c
+ *
+ * Created: 28/02/2017 12:53:32
+ * Author : rmikhaylenko
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
